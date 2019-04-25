89395f02c384380ecdc6b65cc272f612b7f3c65e
Minh Pham <minh213456@hotmail.com>
2019-04-25 02:30:39 -0400

    add close door function

--- Don't See/New Unity Project/Assets/Wall.cs
+++ Don't See/New Unity Project/Assets/Wall.cs
@@ -1,23 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
-public class Wall : MonoBehaviour {
+public class Door : MonoBehaviour {
 	// Start is called before the first frame update
-	public UnityEvent userInput = new UnityEvent();
-	public Transform doorTransform;
 	void Start() {
-		userInput.AddListener(OpenDoor);	
+        	
 	}
 
     	// Update is called once per frame
 	void Update() {
-		if (Input.GetKeyDown("o") && userInput != null){
-			userInput.Invoke();
-		}
-	}
-	void OpenDoor(){
-		doorTransform.localRotation = Quaternion.Euler(0f, 90, 0f);
+        	
 	}
 }
