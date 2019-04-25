6aa7f418ac0671c2dd2f30fd315bc5d0f7a571c2
Minh Pham <minh213456@hotmail.com>
2019-04-25 00:33:47 -0400

    add door and wall

--- Don't See/New Unity Project/Assets/Wall.cs
+++ Don't See/New Unity Project/Assets/Wall.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class Wall : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Wall : MonoBehaviour {
+	// Start is called before the first frame update
+	public UnityEvent userInput = new UnityEvent();
+	public Transform doorTransform;
+	void Start() {
+		userInput.AddListener(OpenDoor);	
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    	// Update is called once per frame
+	void Update() {
+		if (Input.GetKeyDown("o") && userInput != null){
+			userInput.Invoke();
+		}
+	}
+	void OpenDoor(){
+		doorTransform.localRotation = Quaternion.Euler(0f, 90, 0f);
+	}
 }
