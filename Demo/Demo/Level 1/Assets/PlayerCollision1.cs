05b2963cbb243129360d78d4a2f967e4b9f8f502
Kiruthik Sai <ks3738@drexel.edu>
2019-04-26 11:03:03 -0400

    Racing demo

---
+++ Demo/Demo/Level 1/Assets/PlayerCollision1.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollision1 : MonoBehaviour
+{
+    public PlayerMovement movement;     // A reference to our PlayerMovement script
+    public AudioSource walls;
+    void Start()
+    {
+        walls = GetComponent<AudioSource>();
+        Debug.Log("Started");  
+    }
+    private void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        if (collision.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;   // Disable the players movement.
+            FindObjectOfType<GameManager>().EndGame();
+        }
+        if (collision.collider.name == "Barrier")
+        {
+            walls.Play();
+            Debug.Log("Left");
+
+        }
+        if (collision.collider.name == "Barrier1")
+        {
+            walls.Play();
+            Debug.Log("Right");
+
+        }
+    }
+}
