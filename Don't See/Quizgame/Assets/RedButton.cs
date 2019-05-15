4a97d232c250786ba17b52953cc68532c2c55a84
hl664 <hl664@cs.drexel.edu>
2019-05-15 00:40:31 -0400

    quiz game

---
+++ Don't See/Quizgame/Assets/RedButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class RedButton : MonoBehaviour
+{
+
+    int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        score = 0;
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            score++;
+
+
+        }
+    }
+}
