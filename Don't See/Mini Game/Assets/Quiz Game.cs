3276113a0a5fc446b2d1d37c92c0941535092443
hl664 <hl664@cs.drexel.edu>
2019-05-16 02:35:44 -0400

    Tree,grass,rocks,and sound for victory

---
+++ Don't See/Mini Game/Assets/Quiz Game.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Qu : MonoBehaviour
+{
+    private object Console;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Console.writeline('s')
+            Console.readline()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Don't See/New Unity Project/Assets/Congrat.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Congrat : MonoBehaviour
+{
+    public AudioSource Cheer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cheer = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Sound()
+    {
+        Cheer.Play();
+    }
+
+}
