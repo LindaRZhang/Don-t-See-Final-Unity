63b6ec962fcf0c39730f6a6541d216a10a3943e4
Minh Pham <minh213456@hotmail.com>
2019-05-17 08:41:44 -0400

    First attempt trying to merge the changes in Madlib folder. Character don't have gravity or movement

---
+++ Don't See/New Unity Project/Assets/Scripts/CameraFollowCharacter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+/* Linda Rong Zhang
+*  This file is for camera following the player
+*/
+public class CameraFollowCharacter : MonoBehaviour
+{
+    public Transform player; //get like the transform or position of character
+                             //if there is no offset then it is 1st person perspective
+    void Update()
+    {
+        transform.position = player.position;
+        transform.Rotate(0f, 0f, transform.eulerAngles.z * -1);//it will position the camera back down, cancels out
+    }
+}
---
+++ Don't See/New Unity Project/Assets/Scripts/FixPosition.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FixPosition : MonoBehaviour
+{
+    public Text toptext;
+    void Update()
+    {
+        //toptext.position = new Vector3(10f / (float)Screen.width, 10f / (float)Screen.height, 0f);
+    }
+}
