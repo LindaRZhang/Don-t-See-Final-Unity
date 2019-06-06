0f0bef56616ee0e97173b8d40f3a37543f59c46d
Minh Pham <minh213456@hotmail.com>
2019-06-05 22:10:16 -0400

    Adding the saving feature

---
+++ Don-t-See/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+	public string level; // Saving the level
+
+	public void SavePlayer(){
+		SaveSystem.SaveData(this);
+	}
+
+	public void LoadPlayer(){
+		PlayerData data = SaveSystem.LoadData();
+		Vector3 position;
+		position.x = data.playerPosition[0];
+		position.y = data.playerPosition[1];
+		position.z = data.playerPosition[2];
+
+		transform.position = position;
+
+	}
+}
---
+++ Don-t-See/Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem {
+
+	public static string SavePath (){
+		string path = Application.persistentDataPath + "/player.save";
+		return path;
+	}
+	
+
+	public static void SaveData (Player player){
+		BinaryFormatter binary = new BinaryFormatter();
+		string path = SavePath();
+		FileStream stream = new FileStream(path, FileMode.Create);
+		PlayerData data = new PlayerData(player);
+		binary.Serialize(stream, data);
+		stream.Close();
+	}
+
+	public static PlayerData LoadData() {
+		string path = SavePath();
+		if (File.Exists(path)) {
+			BinaryFormatter binary = new BinaryFormatter();
+			FileStream stream = new FileStream(path, FileMode.Open);
+			PlayerData data = binary.Deserialize(stream) as PlayerData;
+			stream.Close();
+
+			return data;
+		}
+		else {
+			Debug.LogError("Save file not found in " + path);
+			return null;
+		}
+
+
+	}
+}
